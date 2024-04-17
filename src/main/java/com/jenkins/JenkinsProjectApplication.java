33ed1074bedf952e26267d66af61ddca7c5270d7
sharathgitha627 <sharathgitha1627@gmail.com>
2024-04-17 12:59:01 +0530

    new jenkins projet is created

---
+++ src/main/java/com/jenkins/JenkinsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectApplication.class, args);
+		System.out.println("standard java project...");
+	}
+
+}
